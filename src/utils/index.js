89eee5a5d475b3f027475dd50cce81745b13b247
mohdfahd <mohammed.f.alsulami@gmail.com>
2024-07-01 11:18:51 -0600

    compress imgs and videos

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,14 +2,14 @@ import hero from "/assets/images/hero.jpeg";
 
 export const heroImg = hero;
 
-import hmv from "/assets/videos/hero.mp4";
-import smallmv from "/assets/videos/smallHero.mp4";
-import highlightFirstmv from "/assets/videos/highlight-first.mp4";
-import highlightSectmv from "/assets/videos/hightlight-third.mp4";
-import highlightThirdmv from "/assets/videos/hightlight-sec.mp4";
-import highlightFourthmv from "/assets/videos/hightlight-fourth.mp4";
-import exploremv from "/assets/videos/explore.mp4";
-import framemv from "/assets/videos/frame.mp4";
+import hmv from "/assets/videos/hero.webm";
+import smallmv from "/assets/videos/smallHero.webm";
+import highlightFirstmv from "/assets/videos/highlight-first.webm";
+import highlightSectmv from "/assets/videos/hightlight-third.webm";
+import highlightThirdmv from "/assets/videos/hightlight-sec.webm";
+import highlightFourthmv from "/assets/videos/hightlight-fourth.webm";
+import exploremv from "/assets/videos/explore.webm";
+import framemv from "/assets/videos/frame.webm";
 
 import apple from "/assets/images/apple.svg";
 import search from "/assets/images/search.svg";
@@ -24,10 +24,10 @@ import yellow from "/assets/images/yellow.jpg";
 import blue from "/assets/images/blue.jpg";
 import white from "/assets/images/white.jpg";
 import black from "/assets/images/black.jpg";
-import explore1 from "/assets/images/explore1.jpg";
-import explore2 from "/assets/images/explore2.jpg";
-import chip from "/assets/images/chip.jpeg";
-import frame from "/assets/images/frame.png";
+import explore1 from "/assets/images/explore1.webp";
+import explore2 from "/assets/images/explore2.webp";
+import chip from "/assets/images/chip.webp";
+import frame from "/assets/images/frame.webp";
 
 export const heroVideo = hmv;
 export const smallHeroVideo = smallmv;
